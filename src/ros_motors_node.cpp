1c4568d95625d71ac58393ae3a4a6dd7bc1f6fdf
freitas-renato <renatinho008@hotmail.com>
2018-10-03 15:02:55 -0300

    Update package dependencies

--- src/ros_motors_node.cpp
+++ src/ros_motors_node.cpp
@@ -27,122 +27,126 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-//#include <thunder_trekking/Motor.h>
-#include "trekking_node/serial.h"
+
+#include <trekking_msgs/Motor.h>
+#include "ros_motors/serial.h"
+#include "ros_motors/errno_string.h"
 
 #define INIT_VMAX 255
-
 #define abs(x) ((x) > 0 ? (x) : -(x))
 
-
-
-class Motor
+class MotorNode
 {
-	public:
-		Motor();
-		void spin();
-		void motors_callback(const thunder_trekking::Motor::ConstPtr& motor_msg);
-	
-	private:
-		ros::NodeHandle nh_;
+  public:
+    MotorNode();
+    void spin();
+    void motors_callback(const trekking_msgs::Motor motor_msg);
 
-		ros::Subscriber motors_sub;
-	
-		void init_serial();
-		void drive_serial();
-		void drive_serial(int servo, int speed);
-	
-		int max_speed = INIT_VMAX;
-		int servo = 0, speed = 0, cur_speed = 80;
-	
-		int fd;
-		const bool use_serial = true;
-		const int baudrate = 9600;
-		
-		void set_max_speed(int new_max_speed);
-		void reset_max_speed();
-		void stop(int servo_offset, int vel);
+  private:
+    ros::NodeHandle nh_;
+    ros::Subscriber motors_sub;
+
+    void init_serial();
+    void drive(int servo, int speed);
+
+    int max_speed = INIT_VMAX;
+    int servo = 0, speed = 0, cur_speed = 80;
+
+    int fd;
+    const bool use_serial = true;
+    const int baudrate = 9600;
+
+    void set_max_speed(int new_max_speed);
+    void reset_max_speed();
+    void stop(int servo_offset, int vel);
+};
+
+MotorNode::MotorNode() : nh_()
+{
+  motors_sub = nh_.subscribe("motor", 1, &MotorNode::motors_callback, this);
+  init_serial();
 }
 
-Motor::Motor() : nh_(){
-	motors_sub = nh_.subscribe("motor", 1, &Motor::motors_callback);
-
+void MotorNode::init_serial()
+{
+  fd = serial_open("/dev/arduino", &baudrate, O_WRONLY);
+  while (fd == -1)
+  {
+    fd = serial_open("/dev/arduino", &baudrate, O_WRONLY);
+    ros::Duration(0.01).sleep();
+  }
 }
 
-void Motor::init_serial(){
-	fd = serial_open("/dev/arduino", &baudrate, O_WRONLY);
-	while (fd == -1){
-		fd = serial_open("/dev/arduino", &baudrate, O_WRONLY);
-		ros::Duration(0.01).sleep();
-	}
+void MotorNode::motors_callback(const trekking_msgs::Motor motor_msg)
+{
+  if (motor_msg.header.stamp == ros::Time(0))
+  {
+    ROS_INFO("Invalid motor message.");
+    return;
+  }
+  servo = motor_msg.servo_angle;
+  speed = motor_msg.speed;
 }
 
-void Motor::drive_serial(int servo, int speed){
-	init_serial();
-	uint8_t message[6] = { 255, servo < 0, abs(servo), speed < 0, abs(speed), 254 };
-	if (write(fd, message, sizeof(message)) == -1){
-		std::cerr << "write: " << errno_string() << std::endl;
-		close(fd);
-		init_serial();
-    }
-	cur_speed = speed;
-}
-void Motor::drive_serial(){
-	drive_serial(servo, speed);
-}
-
-void Motor::set_max_speed(int new_max_speed){
-	max_speed = new_max_speed > 255 ? 255 : new_max_speed;
+void MotorNode::drive(int servo, int speed)
+{
+  init_serial();
+  uint8_t message[6] = { 255, servo < 0, abs(servo), speed < 0, abs(speed), 254 };
+  if (write(fd, message, sizeof(message)) == -1)
+  {
+    std::cerr << "write: " << errno_string() << std::endl;
+    close(fd);
+    init_serial();
+  }
+  cur_speed = speed;
 }
 
-void Motor::reset_max_speed(){
-	max_speed = INIT_VMAX;
+void MotorNode::set_max_speed(int new_max_speed)
+{
+  max_speed = new_max_speed > 255 ? 255 : new_max_speed;
 }
 
-void Motor::motors_callback(const thunder_trekking::Motor::ConstPtr& motor_msg){
-	if (motor_msg -> header.stamp == ros::Time(0))
+void MotorNode::reset_max_speed()
+{
+  max_speed = INIT_VMAX;
+}
+
+void MotorNode::spin()
+{
+  int max_speed_par;
+  nh_.param("max_speed", max_speed_par, INIT_VMAX);
+
+  while (ros::ok())
+  {
+    ros::spinOnce();
+
+    if (nh_.getParam("max_speed", max_speed_par) && max_speed_par != max_speed)
     {
-        ROS_INFO("Invalid motor message.");
-        return;
+      set_max_speed(max_speed_par);
     }
-	servo = motor_msg -> servo;
-	speed = motor_msg -> motor;
+    if (use_serial)
+    {
+      drive(this->servo, this->speed);
+    }
+  }
 }
 
-void Motor::spin(){
-
-	uint8_t max_speed_par;
-	nh_.param("max_speed", max_speed_par, INIT_VMAX);
-	
-	while(ros::ok()){
-		ros::spinOnce();
-		
-		if(nh_.getParam("max_speed", max_speed_par) && max_speed_par != max_speed){
-			set_max_speed(max_speed_par);
-		}
-
-		if(use_serial){
-			drive_serial();
-		}
-	}
-}
-
-void Motor::stop(int servo_offset, int vel)
+void MotorNode::stop(int servo_offset, int vel)
 {
-	cur_speed = vel;
-	while (cur_speed != 0)
-	{
-		drive_serial(servo_offset, cur_speed--);
-		ros::Duration(0.01).sleep();
-	}
+  cur_speed = vel;
+  while (cur_speed != 0)
+  {
+    drive(servo_offset, cur_speed--);
+    ros::Duration(0.01).sleep();
+  }
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "motors_subscriber");
-  
-  Motor motor;
+
+  MotorNode motor;
   motor.spin();
-  
+
   return 0;
 }
